b17b5753c380c906bf805d90db2b896ff61d1d03
Luis Eduardo <loistronics@gmail.com>
2023-12-25 23:05:43 -0400

    Configuracion TKinter
    Se realiza la configuracion de la ventana TKinter

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+aplicacion = Tk() #Se inicializa TKinter
+
+aplicacion.geometry("1020x630+0+0")  #Se cofigura tamano de la ventana
+
+aplicacion.resizable(0,0) #Ni en el eje x ni en el eje y se podra expandir la ventana
+
+aplicacion.title("Restaurant xxx - Sistema de Facturacion")
+
+aplicacion.config(bg='azure') #Se configura color de fondo de la ventana
+
+aplicacion.mainloop() #Evita que la pantalla se cierre
+
